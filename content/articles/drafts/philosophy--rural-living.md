6169e391e4f1d6e1963edd8a6389472a5d1d7fad
rwev <rwev@protonmail.ch>
2019-11-22 12:27:16 -0700

    migrate personal content from rwev.gitlab.io; use mesa theme

---
+++ content/articles/drafts/philosophy--rural-living.md
@@ -0,0 +1,18 @@
+Title: Rural Living
+Category: philosophy
+Tags: rural,life,outdoors,peace
+
+## Places aren't lonely -- people are
+
+## Living _in_ the world, not _of_ it
+
+## Staying within Means
+
+## True Freedom and Individuality - No Crushing Peer Pressure / Gov't
+
+## De-Monetization of Daily Life
+
+## Freedom is Wealth
+
+## An End, Not a Mean
+Fisherman and the Businessman
---
+++ pelicanconf.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+from __future__ import unicode_literals
+
+import sys
+import os
+
+sys.path.append(os.curdir)
+from mesa import *
+
+# TEMPLATE
+OWNER = "rwev"
+AUTHOR = OWNER
+SITENAME = "coyote.life"
+
+SITEURL = "localhost:8000"
+
+USER_LOGO_URL = "assets/images/valknut-nord7.png"
+TAGLINE = "Wild wolf before domesticated dog."
+
+DISPLAY_PAGES_ON_MENU = False
+# BUILD PROCESS
+DEFAULT_METADATA = {
+    "author": OWNER,
+    "status": "draft",  # status: published, draft, hidden
+}
+
+PATH = "content"
+PAGE_PATHS = ["pages"]
+STATIC_PATHS = ["assets"]
+ARTICLE_PATHS = ["articles/published"]
+OUTPUT_PATH = "./public"  # for gitlab page
+
+TIMEZONE = "America/Denver"
+DEFAULT_LANG = "en"
+
+LINK_ITEMS = (
+    ("GitHub", "https://github.com/rwev"),
+    ("GitLab", "https://gitlab.com/rwev"),
+)
+
+# PLUGINS
+PLUGIN_PATHS = ["pelican-plugins"]
+PLUGINS = [
+    "filetime_from_git",
+    "autopages",
+    "similar_posts",
+    "neighbors",
+    "section_number",
+    #    "encrypt_content", TODO
+    "more_categories",
+    "gzip_cache",
+    "photos",
+    "summary",
+    "clean_summary",
+]
+
+# autopages
+AUTOPAGES_PATH = os.path.join("content", "autopages")
+
+AUTHOR_PAGE_PATH = os.path.join(AUTOPAGES_PATH, "authors")
+CATEGORY_PAGE_PATH = os.path.join(AUTOPAGES_PATH, "categories")
+TAG_PAGE_PATH = os.path.join(AUTOPAGES_PATH, "tags")
+
+
+# encrypt_content
+ENCRYPT_CONTENT = {
+    "title_prefix": "[Encrypted]",
+    "summary": "This content is password protected.",
+}
+
+# photos
+PHOTO_LIBRARY = "./content/assets/images"
+PHOTO_EXIF_COPYRIGHT_AUTHOR = "Ryan William"
+
